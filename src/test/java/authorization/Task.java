bc1d4aaa5141028eecb08b60cb7d65d3657595ce
Maria Sukhinina <mariarozanova91@gmail.com>
2022-10-11 15:59:32 +0300

    refactored

--- src/test/java/authorization/Task.java
+++ src/test/java/authorization/Task.java
@@ -1,7 +1,5 @@
 package authorization;
 
-//import com.fasterxml.jackson.annotation.JsonCreator;
-//import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
